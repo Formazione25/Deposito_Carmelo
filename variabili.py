4c92f3a9e34d95d5c9d13bbd753286096ee45fbe
Formazione25 <viaggi537@gmail.com>
2025-04-07 17:46:00 +0200

    Create variabili.py

---
+++ variabili.py
@@ -0,0 +1,21 @@
+#tipi numerici
+intero= int(input("inserisci intero: "))
+virgola= float(input("inserisci numero virgola: "))
+print(intero, virgola)
+
+#tipi semantici
+stringa= input("inserisci parola")
+s= "Ciao, mondo!"
+print(len(s))
+print(s.upper())
+print(s.split(','))
+print(s.replace("mondo", "universo"))
+
+#booleani
+x = 5
+y = 10
+z= 7
+
+print(x<y and y>z)
+print (x<y or z>y)
+print(not(x<y))
