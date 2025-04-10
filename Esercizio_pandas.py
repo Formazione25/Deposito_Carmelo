4208636220a692cd20647c8cc16c85b786717ae0
Formazione25 <viaggi537@gmail.com>
2025-04-10 14:24:14 +0200

    Add files via upload

--- Esercizio_pandas.py
+++ Esercizio_pandas.py
@@ -9,9 +9,9 @@ print(Salario_random)
 
 data = {
     "Nome": ['Alice', 'Bob', 'Carla', 'Bob', 'Carla', 'Alice'],
-    'Età': [20, 28, 36, 44, 52, 60],
+    'Età': np.linspace(20,60,6,dtype="int64"),
     'Città': ['Roma', 'Milano', 'Napoli', 'Milano', 'Napoli', 'Roma'],
-    'Salario': [0,  200,  400,  600,  800, 1000]
+    'Salario': np.linspace(0,1000,6,dtype="int64")
     }
 
 print(data)
@@ -33,12 +33,26 @@ print(mediana_età_età)
 std_età= df['Età'].std()
 print(std_età)
 
+#calcolo media mediana e std
+media_salario =df['Salario'].mean()
+print(media_salario)
+mediana_salario =df['Salario'].median()
+print(mediana_salario)
+std_salario= df['Salario'].std()
+print(std_salario)
+
 #rimozione duplicati
 df = df.drop_duplicates()
 print(df)
 
 # sostituzione
 df['Età'].fillna(df['Età'].median(), inplace= True)
+
+# Aggiungiamo una nuova colonna
+df['Categoria_Età'] = np.where(df['Età'] < 18, 'Giovane',
+                               np.where(df['Età'] <=95, 'Adulto', 'Senior'))
+
+print(df)
 
 
 
