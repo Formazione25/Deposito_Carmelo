d7d9d9dac0c7fc5443f3ca0c29a9bb0d801f49ed
Formazione25 <viaggi537@gmail.com>
2025-04-11 16:38:04 +0200

    Add files via upload

--- Esercizi/Esercizio_sqlite3.py
+++ Esercizi/Esercizio_sqlite3.py
@@ -1,6 +1,6 @@
 import sqlite3 
 
-
+#creiamo database
 conn = sqlite3.connect('scuola.db')
 cur = conn.cursor()
 cur.execute('''
@@ -9,15 +9,28 @@ cur.execute('''
         nome TEXT
     )
 ''')
+#creo ciclo che permette di scegliere nomi o rimuovere
 while True:
-    nome_inserito = input("inserisci nome o digita basta")
-    if nome_inserito != "basta":
+    cosa_fare= input("cosa vuoi fare? Digita aggiungo, rimuovi o basta")
+    if cosa_fare == "aggiungo":
+        nome_inserito = input("inserisci nome")
         cur.execute("INSERT INTO studenti (nome) VALUES (?)",(nome_inserito,))
         conn.commit()
         cur.execute("SELECT * FROM studenti")
         righe = cur.fetchall()
         for r in righe:
             print(r)
+    elif cosa_fare == "rimuovi":
+        nome_da_rimuovere= input("nome da rimuovere dalla tabella? ")
+        cur.execute("DELETE FROM studenti WHERE nome = (?)",(nome_da_rimuovere,))
+        conn.commit()
+        righe = cur.fetchall()
+        for r in righe:
+            print(r)
     else:
         break
+
+
+
+
 
